364fdfb0c5e8ab35a4d355c19ee096b3cd8e5499
erenhncr <erenhncr@gmail.com>
2022-06-14 17:17:20 +0300

    update stylint for ignore css module files

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import styles from './styles.module.css';
+
 type TestProps = {
   text: boolean;
 };
 
 function Test({ text }: TestProps) {
   return (
-    <aside>
+    <aside className={styles.testCSSModules}>
       <input type='text' />
       {text}
     </aside>
